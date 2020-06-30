46a9ea546ca3cd31baf2bf22b0f8a9fba2838253
nekisse <lsh891224@gmail.com>
2020-06-30 15:19:48 +0900

    p.115

---
+++ src/main/java/com/nekisse/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,42 @@
+package com.nekisse.book.springboot.service.posts;
+
+import com.nekisse.book.springboot.domain.posts.Posts;
+import com.nekisse.book.springboot.domain.posts.PostsRepository;
+import com.nekisse.book.springboot.web.dto.PostsResponseDto;
+import com.nekisse.book.springboot.web.dto.PostsSaveRequestDto;
+import com.nekisse.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts =
+                postsRepository
+                        .findById(id)
+                        .orElseThrow(() -> new IllegalAccessError("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalAccessError("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+}
